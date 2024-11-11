e0f4a98661fc3597ddeaa5b42c832b009955c394
seldesoky <sameh.s.eldesoky@gmail.com>
2024-11-10 19:42:45 -0500

    class 2 unit 4

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Cat
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cat_index(request):
+    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()  # look familiar?
+    return render(request, 'cats/index.html', {'cats': cats})
